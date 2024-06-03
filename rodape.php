aa2f352cfb4b7dd21ab8e59e465dd53587f7e7e3
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2024-06-03 16:41:02 -0300

    up

---
+++ rodape.php
@@ -0,0 +1,32 @@
+
+</body>
+<!-- Footer -->
+<footer class="bg-light text-center text-lg-start w-100">
+    <div class="container p-4 d-flex justify-content-between">
+        <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Meu Site</h5>
+            <p>
+                Este é um exemplo de pagina usando HTML CSS JS PHP e Bootstrap <br> para o Projeto Integrador da turma T.I-22.
+            </p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0 text-right ">
+            <h5 class="text-uppercase">Icones</h5>
+            <ul class="list-unstyled mb-0">
+                <li>
+                    <a href="#!" class="text-dark">redes sociais</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="text-center p-3 bg-dark text-white w-100">
+        © 2024 Projeto Integrador T.I-22
+    </div>
+</footer>
+
+
+<!-- Bootstrap JS e dependências -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</html>
