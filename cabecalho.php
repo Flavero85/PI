6e9b7267b33f49ce0bf55784ba06b42100ef57e1
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2024-06-05 13:19:22 -0300

    up

--- cabecalho.php
+++ cabecalho.php
@@ -12,13 +12,8 @@
 <!-- Menu Responsivo -->
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<<<<<<< HEAD
 <a class="navbar-brand " href=""><img src="img/logo.png" alt="" style="width: 300px; height: auto;"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="pi.php" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-=======
-    <a class="navbar-brand" href=""><img src="img/logo.png" alt=""></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
->>>>>>> da8520ae6c62cd6f875ee70e6f3718f2255f4fe3
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
